c8b89b3f4469ed6d788820025f0bcee1104a77e6
Тимошкин Андрей <ftinkere@ya.ru>
2023-09-21 20:35:23 +0300

    phonetic removed empty sub_columns

--- app/Services/TableService.php
+++ app/Services/TableService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services;
 
+use Illuminate\Database\Eloquent\Collection;
+
 class TableService
 {
-    function tables($language, $data, $meta) {
+    function tables($language, Collection $data, $meta) {
         $tables = [];
 
 
@@ -19,10 +21,20 @@ class TableService
             $first_row = [];
             $first_row[] = ['data' => $table_name, 'header' => true];
             foreach ($table_meta['columns'] as $column_name) {
+                $colspan = count($table_meta['sub_columns']);
+
+                $sub_column_names = $table_meta['sub_columns']->isEmpty() ? [null] : $table_meta['sub_columns'];
+                foreach ($sub_column_names as $sub_column_name) {
+                    $all_sub_column_data = $table_data->where('column', $column_name)
+                        ->whereIn('sub_column', [$sub_column_name, null]);
+                    if ($all_sub_column_data->isEmpty()) {
+                        $colspan -= 1;
+                    }
+                }
                 $first_row[] = [
                     'data' => $column_name,
                     'header' => true,
-                    'colspan' => count($table_meta['sub_columns']),
+                    'colspan' => $colspan,
                 ];
             }
             $table['rows'][] = $first_row;
@@ -34,11 +46,18 @@ class TableService
 
                 foreach ($table_meta['columns'] as $column_name) {
                     $column_data = $row_data->where('column', '=', $column_name);
-                    $sub_column_names = $table_meta['sub_columns']->isEmpty() ? ['sub'] : $table_meta['sub_columns'];
+                    $sub_column_names = $table_meta['sub_columns']->isEmpty() ? [null] : $table_meta['sub_columns'];
 
                     foreach ($sub_column_names as $sub_column_name) {
                         $sub_column_data = $column_data->where('sub_column', '=', $sub_column_name);
                         $td_data = $sub_column_data->isNotEmpty() ? $sub_column_data->first() : ($column_data->first()?->sub_column == null ? $column_data->first() : null);
+
+                        $all_sub_column_data = $table_data->where('column', $column_name)
+                            ->whereIn('sub_column', [$sub_column_name, null]);
+                        if ($all_sub_column_data->isEmpty()) {
+                            continue;
+                        }
+
                         $colspan = 1;
                         if ($td_data && $td_data->sub_column == null) {
                             $colspan = count($table_meta['sub_columns']) > 0 ? count($table_meta['sub_columns']) : 1;
